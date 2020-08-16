fdf7971e376202446dfee3a522f7260bf3ba7bd1
Jonathas Germano <jonathas.germano@hotmail.com>
2020-08-17 02:02:16 +0530

    Added logic to create new teacher and classes in the database

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-// app.get('/users', (req, res) => {
-//     const users = [
-//         { name: 'Jonathas', age: 25 },
-//         { name: 'Ilehana', age: 24 },
-//         { name: 'William', age: 23 },
-//         { name: 'Beatriz', age: 22 },
-//         { name: 'Pereira', age: 74 },
-
-//     ]
-
-//     return res.json(users);
-// } )
-
-app.post('/users', (req, res) => {
-    const users = [
-        { name: 'Jonathas', age: 25 },
-        { name: 'Ilehana', age: 24 },
-        { name: 'William', age: 23 },
-        { name: 'Beatriz', age: 22 },
-        { name: 'Pereira', age: 74 },
-
-    ]
-
-    return res.json(users);
-} )
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333)
